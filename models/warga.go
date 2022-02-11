9c5253b991d2cedb9b6c5528132ddb4d98f4af4d
erb4f1dotama <erb4fidotama@gmail.com>
2022-02-11 19:20:46 +0700

    perbaikan model warga

--- models/warga.go
+++ models/warga.go
@@ -8,11 +8,16 @@ import (
 
 type Warga struct {
 	gorm.Model
-	NamaWarga    string
-	NoKk         string
-	Nik          string
-	Alamat       string
-	TypeKeluarga int
-	TanggalLahir time.Time `json:"tanggal_lahir"`
-	TempatLahir  string
+	NamaWarga                    string
+	KelaminLookupDetailID        int
+	NoKk                         string
+	Nik                          string
+	Alamat                       string
+	RtLookupDetailID             int
+	RwLookupDetailID             int
+	NoTelp                       string
+	StatusKeluargaLookupDetailID int
+	TanggalLahir                 time.Time `json:"tanggal_lahir"`
+	TempatLahir                  string
+	LulusPosyandu                bool
 }
