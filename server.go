c1a16f67463125397f7c5b5a550d6eb81887f496
erb4f1dotama <erb4fidotama@gmail.com>
2022-02-12 12:11:50 +0700

    perbaikan routes

--- server.go
+++ server.go
@@ -15,22 +15,34 @@ func main() {
 
 	router := gin.Default()
 
-	v1 := router.Group("api/v1") // /api/v1
+	router.POST("/login", routes.Login)
+	admin := router.Group("/admin") //admin is the prefix
 	{
-		v1.POST("/login", routes.Login) // /api/v1/login
-		admin := v1.Group("/admin")     // /api/v1/sewaAset
-		{
-			admin.GET("/account", routes.GetAccount)   // /api/v1/admin/account
-			admin.POST("/account", routes.PostAccount) // /api/v1/admin/account
-		}
-		// v1.POST("/warga", middleware.IsAuth(), routes.PostWarga)
-		// v1.GET("/warga", middleware.IsAuth(), routes.GetWarga)
-		v1.POST("/lookup", routes.PostLookup)
-		v1.GET("/lookup", routes.GetLookup)
-		v1.GET("/lookup/:lookup_uuid", routes.GetLookupByUuid)
-		v1.PUT("/lookup/:lookup_uuid", routes.PutLookup)
-		v1.POST("/lookupDetail", routes.PostLookupDetail)
+		admin.GET("/account", routes.GetAccount)   // /admin/account
+		admin.POST("/account", routes.PostAccount) // /admin/account
+	}
+	lookup := router.Group("/lookup") //lookup is the prefix
+	{
+		lookup.GET("/", routes.GetLookup)                   // /lookup
+		lookup.GET("/:lookup_uuid", routes.GetLookupByUuid) // /lookup/:lookup_uuid
+		lookup.POST("/", routes.PostLookup)                 // /lookup
+		lookup.PUT("/:lookup_uuid", routes.PutLookup)       // /lookup/:lookup_uuid
+	}
+	lookupDetail := router.Group("/lookup_detail") //lookup_detail is the prefix
+	{
+		lookupDetail.GET("/", routes.GetLookupDetail)                          // /lookup_detail
+		lookupDetail.GET("/:lookup_detail_uuid", routes.GetLookupDetailByUuid) // /lookup_detail/:lookup_detail_uuid
+		lookupDetail.POST("/", routes.PostLookupDetail)                        // /lookup_detail
+		lookupDetail.PUT("/:lookup_detail_uuid", routes.PutLookupDetail)       // /lookup_detail/:lookup_detail_uuid
+	}
+	warga := router.Group("/warga") //warga is the prefix
+	{
+		warga.GET("/", routes.GetWarga) // /warga
+		// warga.GET("/:warga_uuid", routes.GetWargaByUuid) // /warga/:warga_uuid
+		warga.POST("/", routes.PostWarga) // /warga
+		// warga.PUT("/:warga_uuid", routes.PutWarga)       // /warga/:warga_uuid
 	}
 
+	// router.Run(":8080") // if you want to run on port 8080
 	router.Run()
 }
